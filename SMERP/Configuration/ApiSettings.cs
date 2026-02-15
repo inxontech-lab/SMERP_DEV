c9420d5df8153005aadc0ff216cf4d69d73cf90a
inxontech-lab <inxon.tech@gmail.com>
2026-02-15 15:42:53 +0300

    test

--- SMERP/Configuration/ApiSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace SMERP.Configuration;
-
-public sealed class ApiSettings
-{
-    public const string SectionName = "ApiSettings";
-
-    public string BaseUrl { get; set; } = string.Empty;
-}
--- SMERP/services/SaasServices/SaasApiClientFactory.cs
+++
@@ -1,6 +0,0 @@
-namespace SMERP.services.SaasServices;
-
-public static class SaasApiClientFactory
-{
-    public const string ClientName = "SaasApiClient";
-}
