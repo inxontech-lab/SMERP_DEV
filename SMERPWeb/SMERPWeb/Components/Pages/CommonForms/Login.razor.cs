a320f5327d6568a211cccfb0764bf5b46f49c50b
inxontech-lab <inxon.tech@gmail.com>
2026-02-17 08:47:31 +0300

    Navigate to home page on login button click

--- SMERPWeb/SMERPWeb/Components/Pages/CommonForms/Login.razor.cs
+++ SMERPWeb/SMERPWeb/Components/Pages/CommonForms/Login.razor.cs
@@ -4,9 +4,17 @@ namespace SMERPWeb.Components.Pages.CommonForms;
 
 public partial class Login : ComponentBase
 {
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = default!;
+
     protected string TenantEmail { get; set; } = string.Empty;
 
     protected string UserName { get; set; } = string.Empty;
 
     protected string Password { get; set; } = string.Empty;
+
+    protected void OnLoginClicked()
+    {
+        NavigationManager.NavigateTo("/Home");
+    }
 }
