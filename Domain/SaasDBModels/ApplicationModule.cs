7737cd90fc3925d8f446bb15527dec30eabf16d3
inxontech-lab <inxon.tech@gmail.com>
2026-02-17 09:00:10 +0300

    Db update

---
+++ Domain/SaasDBModels/ApplicationModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.SaasDBModels;
+
+public partial class ApplicationModule
+{
+    public int Id { get; set; }
+
+    public string ModuleNameEn { get; set; } = null!;
+
+    public string? ModuleNameAr { get; set; }
+
+    public bool Active { get; set; }
+}
---
+++ Domain/SaasDBModels/TenantModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.SaasDBModels;
+
+public partial class TenantModule
+{
+    public int Id { get; set; }
+
+    public int TenantId { get; set; }
+
+    public string ModuleId { get; set; } = null!;
+
+    public bool Active { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
